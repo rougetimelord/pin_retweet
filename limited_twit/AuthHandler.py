4d014358f95550326709269b26ecb23459276436
Rouge <rougetimelord@users.noreply.github.com>
2022-09-13 19:55:41 -0700

    Refactor names

--- limited_twit/AuthHandler.py
+++ limited_twit/AuthHandler.py
@@ -13,28 +13,30 @@ class AuthHandler:
         self,
         consumer_key: str,
         consumer_secret: str,
-        user_key: str | None,
-        user_secret: str | None,
+        access_token: str | None,
+        access_token_secret: str | None,
         callback: str | None,
     ) -> None:
-        self.con_key = consumer_key
-        self.con_secret = consumer_secret
-        if user_key == None or user_secret == None:
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        if access_token == None or access_token_secret == None:
             raise AuthException("Provide a user token")
-        self.user_key = user_key
-        self.user_secret = user_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
         self.callback = callback
+        self.username = None
+        self.request_token = {}
         self.oauth = OAuth1Session(
             consumer_key,
             client_secret=consumer_secret,
             callback_uri=self.callback,
         )
 
-    def apply_auth(self) -> OAuth1:
+    def apply_auth(self):
         return OAuth1(
-            self.con_key,
-            client_secret=self.con_secret,
-            resource_owner_key=self.user_key,
-            resource_owner_secret=self.user_secret,
+            self.consumer_key,
+            client_secret=self.consumer_secret,
+            resource_owner_key=self.access_token,
+            resource_owner_secret=self.access_token_secret,
             decoding=None,
         )
