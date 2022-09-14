192140b988a1c472260373f1f33173a16d7f01bb
Rouge <rougetimelord@users.noreply.github.com>
2022-09-13 20:00:07 -0700

    Add main to run the script

---
+++ main.py
@@ -0,0 +1,21 @@
+import limited_twit, json
+
+
+def main():
+    with open("key.json", "r") as f:
+        keys = json.load(f)
+    client = limited_twit.Client(
+        keys["consumer_key"],
+        keys["consumer_secret"],
+        keys["access_token"],
+        keys["access_token_secret"],
+    )
+    id_str = input("Tweet link: ").split("/")[-1]
+    try:
+        resp = client.retweet_then_pin(id_str)
+    except limited_twit.ClientException as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
